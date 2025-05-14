deeaceb48b8f90b6871b5edc338e9b5503d5ba09
PINDICHI Elena (pindiche) <pindiche@esisar.inpg.fr>
2025-05-14 15:32:15 +0200

    introducing casadi in simulink

--- MPC_nonlinear_ref_tracking.m
+++ MPC_nonlinear_ref_tracking.m
@@ -2,6 +2,8 @@ clc; close all; clear;
 
 % Load CasADi
 addpath('C:\Users\pindiche\Documents\MATLAB\casadi-3.6.7-windows64-matlab2018b\')
+setenv('PATH', [getenv('PATH') ';' 'C:\Users\pindiche\Documents\MATLAB\casadi-3.6.7-windows64-matlab2018b\'])
+
 % addpath('C:\Users\Elena\Documents\MATLAB\casadi-3.6.6-windows64-matlab2018b\')
 % addpath('C:\Users\bajdus\Documents\Qcar2\Collision-Avoidance-Algorithm\SteeringWheelRobot\casadi-3.6.7-windows64-matlab2018b\')
 import casadi.*;
